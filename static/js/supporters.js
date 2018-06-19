4010f1a8106b243eb0d5bbb986b75b74a3c2ca2a
Tibor Maron <tibormaron@gmail.com>
2018-06-19 20:03:54 +0200

    finished supporters

--- static/js/supporters.js
+++ static/js/supporters.js
@@ -1,49 +1,57 @@
-var nodes = null;
-var edges = null;
-var network = null;
-
 // Called when the Visualization API is loaded.
 function draw() {
 
     // create nodes with images
     let DIR = '/static/images/';
-    nodes = [
+    let nodes = [
+        {id: 0, size: 70, shape: 'circle', label: 'Social media'},
         {id: 1, shape: 'circularImage', image: DIR + 'facebook.png', url: 'https://facebook.com'},
         {id: 2, shape: 'circularImage', image: DIR + 'instagram.png', url: 'https://instagram.com'},
         {id: 3, shape: 'circularImage', image: DIR + 'twitter.png', url: 'https://twitter.com'},
         {id: 4, shape: 'circularImage', image: DIR + 'youtube.png', url: 'https://youtube.com'},
+        {id: 5, shape: 'circularImage', image: DIR + 'snapchat.png', url: 'https://snapchat.com'},
     ];
 
     // create connections
-    edges = [
-        {from: 1, to: 2},
-        {from: 1, to: 4},
-        {from: 2, to: 3},
-        {from: 3, to: 4},
+    let edges = [
+        {from: 0, to: 1},
+        {from: 0, to: 2},
+        {from: 0, to: 3},
+        {from: 0, to: 4},
+        {from: 0, to: 5},
     ];
 
     // create a network
-    var container = document.getElementById('mynetwork');
-    var data = {
+    let container = document.getElementById('mynetwork');
+    let data = {
         nodes: nodes,
         edges: edges
     };
-    var options = {
+    let options = {
         nodes: {
             borderWidth: 2,
             size: 40,
             color: {
-                border: '#222222',
-                background: 'white'
+                border: 'black',
+                background: 'white',
+                hover: {
+                    border: 'grey'
+                }
             },
-            font: {color: '#eeeeee'}
+            font: {color: 'black'}
         },
         edges: {
-            color: 'lightgray'
+            color: 'black'
+        },
+        interaction: {
+            dragNodes: false,
+            dragView: false,
+            zoomView: false,
+            hover: true,
         }
     };
 
-    network = new vis.Network(container, data, options);
+    let network = new vis.Network(container, data, options);
 
     // click event opens a new tab
     network.on('click', function (properties) {
