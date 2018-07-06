37f879dd65f901255cd77c73ea3bff864dd7fd2f
Unknown <regoczi.tamas01@gmail.com>
2018-07-06 15:14:13 +0200

    responsive navbar implemented

---
+++ static/js/navbar.js
@@ -0,0 +1,7 @@
+function openSlideMenu() {
+    document.getElementById("side-menu").style.width = "250px";
+}
+
+function closeSideMenu() {
+    document.getElementById("side-menu").style.width = "0px";
+}
